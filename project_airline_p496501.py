099113ae951a06e2dccb68210ea423a936f2a7a6
xuzikang666 <2322816247@qq.com>
2026-03-17 19:15:25 +0800

    Create project_airline_p496501.py
    the code include check seats, book seats, free seats, show seats' status, exit, and cancel seats

---
+++ project_airline_p496501.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 15 23:06:05 2026
+
+@author: xuzikang
+"""
+
+
+rows = 80
+column = ["A","B","C","D","E","F"]
+seats = {}
+
+for r in range(1, rows+1):
+    for c in column:
+        seats[str(r)+c] = "Free"
+ 
+def check_seat():
+    seat = input("please enter the seat number: ")
+    if seat in seats:
+        if seats[seat] == "Free":
+            print("It is free seat")
+        else:
+            print("seat is booked")
+    else:
+        print("number is wrong")
+        
+def book_seat():
+    seat = input("please enter the seat number: ")
+    if seat in seats:
+        if seats[seat] == "Free":
+            print("It has booked successfully")
+        else:
+            print("It has been booked")
+    else:
+        print("number is wrong")
+        
+def free_seat():
+    seat = input("please enter the seat number: ")
+    if seat in seats:
+        if seats[seat] != "Free":
+            print("free it successfully")
+        else:
+            print("It idon't need free")
+    else:
+        print("number is wrong")
+        
+def show_status():
+    for seat, status in seats.items():
+        print(seat, status)
+        
+def cancel_booking():
+    seat = input("please enter the seat number: ")
+    if seat in seats:
+        if seats[seat] == "Received":
+            print("seat has been cancelled")
+        else:
+            print("seat don't be booked")
+    else:
+        print("number is wrong")
+        
+while True:
+    print("choose aseat system")
+    print("1.check seat")
+    print("2.book seat")
+    print("3.free seat")
+    print("4.show seat status")
+    print("5.exit")
+    print("6.cancel seat")
+    
+    choice = input("choose a number: ")
+    if choice == "1":
+        check_seat()
+    elif choice == "2":
+        book_seat()
+    elif choice == "3":
+        free_seat()
+    elif choice == "4":
+        show_status()
+    elif choice == "5":
+        break
+    elif choice == "6":
+        cancel_booking()
+    else:
+        print("Wrong input")
+        
+    
+
